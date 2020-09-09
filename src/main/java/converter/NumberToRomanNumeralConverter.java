dde6f42fe760420828afd267026af52f78c765ed
tiba666 <frehr@hotmail.com>
2020-09-09 12:51:05 +0200

    converter done

--- src/main/java/converter/NumberToRomanNumeralConverter.java
+++ src/main/java/converter/NumberToRomanNumeralConverter.java
@@ -1,7 +1,96 @@
 package converter;
 
+
+
 public class NumberToRomanNumeralConverter {
+
     public String convert(int i) {
-        return null;
+        if(i<1){
+            return "the lowest number we support is 1";
+        }
+        if(i>3999){
+            return "we do not support a numbere there is bigger then 3999";
+        }
+        String result ="";
+
+        switch (String.valueOf(i).length()){
+            case 4:
+                result += thousands(i/1000);
+                i = i%1000;
+            case 3:
+
+                result += hundreds(i/100);
+                i = i%100;
+            case 2:
+
+                result += tens(i/10);
+                i = i%10;
+            case 1:
+                result += onesToRoman(i/1);
+        }
+
+        return result;
+    }
+
+    private String onesToRoman(int i ){
+        switch (i){
+            case 9:
+                return "IX";
+            case 8:
+                return "VIII";
+            case 7:
+                return "VII";
+            case 6:
+                return "VI";
+            case 5:
+                return "V";
+            case 4:
+                return "IV";
+            default:
+                return "III".substring(0,i);
+        }
+    }
+    private String tens(int i ){
+        switch (i){
+            case 9:
+                return "XC";
+            case 8:
+                return "LXXX";
+            case 7:
+                return "LXX";
+            case 6:
+                return "LX";
+            case 5:
+                return "L";
+            case 4:
+                return "XL";
+            default:
+                return "XXX".substring(0,i);
+        }
+    }
+    private String hundreds(int i ){
+        switch (i){
+            case 9:
+                return "CM";
+            case 8:
+                return "DCCC";
+            case 7:
+                return "DCC";
+            case 6:
+                return "DC";
+            case 5:
+                return "D";
+            case 4:
+                return "CD";
+            default:
+                return "CCC".substring(0,i);
+        }
+    }
+    private String thousands(int i){
+        switch (i){
+
+            default:
+                return ("MMM").substring(0,i);
+        }
     }
 }
--- src/test/java/converter/NumberToRomanNumeralTest.java
+++ src/test/java/converter/NumberToRomanNumeralTest.java
@@ -1,10 +1,10 @@
 package converter;
 
-import converter.NumberToRomanNumeralConverter;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NumberToRomanNumeralTest {
@@ -17,6 +17,281 @@ public class NumberToRomanNumeralTest {
         NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
 
         // Assert
-
+        assertTrue(c instanceof NumberToRomanNumeralConverter);
     }
+
+    @Test
+    public void mustBeIWhemConvert1() {
+
+        // Arrange
+        var input = 1;
+        var expected = "I";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeIIWhenConvert2() {
+
+        // Arrange
+        var input = 2;
+        var expected = "II";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeIIIWhenConvert3() {
+
+        // Arrange
+        var input = 3;
+        var expected = "III";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeIVWhenConvert4() {
+
+        // Arrange
+        var input = 4;
+        var expected = "IV";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeVWhenConvert5() {
+
+        // Arrange
+        var input = 5;
+        var expected = "V";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeVIWhenConvert6() {
+
+        // Arrange
+        var input = 6;
+        var expected = "VI";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeIXWhenConvert9() {
+
+        // Arrange
+        var input = 9;
+        var expected = "IX";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeXWhenConvert10() {
+
+        // Arrange
+        var input = 10;
+        var expected = "X";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeXXXIXWhenConvert39() {
+
+        // Arrange
+        var input = 39;
+        var expected = "XXXIX";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeXXXWhenConvert30() {
+
+        // Arrange
+        var input = 30;
+        var expected = "XXX";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeLXXXWhenConvert80() {
+
+        // Arrange
+        var input = 80;
+        var expected = "LXXX";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    public void mustBeXCWhenConvert90() {
+
+        // Arrange
+        var input = 90;
+        var expected = "XC";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeCCCIWhenConvert301() {
+
+        // Arrange
+        var input = 301;
+        var expected = "CCCI";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeCDWhenConvert400() {
+
+        // Arrange
+        var input = 400;
+        var expected = "CD";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeDWhenConvert500() {
+
+        // Arrange
+        var input = 500;
+        var expected = "D";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeDCCCWhenConvert800() {
+
+        // Arrange
+        var input = 800;
+        var expected = "DCCC";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeCMWhenConvert900() {
+
+        // Arrange
+        var input = 900;
+        var expected = "CM";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeMWhenConvert1000() {
+
+        // Arrange
+        var input = 1000;
+        var expected = "M";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeMMMWhenConvert3000() {
+
+        // Arrange
+        var input = 3000;
+        var expected = "MMM";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeMMMCMXCIXWhenConvert3999() {
+
+        // Arrange
+        var input = 3999;
+        var expected = "MMMCMXCIX";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void mustBeDCCCLXXXVIIIWhenConvert888() {
+
+        // Arrange
+        var input = 888;
+        var expected = "DCCCLXXXVIII";
+        // Act
+        NumberToRomanNumeralConverter c = new NumberToRomanNumeralConverter();
+        var result = c.convert(input);
+
+        assertEquals(expected, result);
+    }
+
 }
